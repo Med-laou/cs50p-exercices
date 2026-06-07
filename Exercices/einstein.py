fe9b3f3786d459d5f96ead45eee6ef45f2d38b0a
Laouamer Med <laouamer.med@gmail.com>
2026-06-07 21:49:27 +0200

    CS50P Lecture 0 - einstein

---
+++ Exercices/einstein.py
@@ -0,0 +1,6 @@
+# Saiser une masse en Kg
+
+masse = int(input("Saisir une masse en Kg : "))
+vitesse_lumiere = 300000000  # en m/s
+E = masse * vitesse_lumiere ** 2  # Calcul de l'énergie en joules
+print(f"{E} joules") 
